b31ddfac0069719b1e6e5ce9aaef7338035226c5
kinglg <king@liugang.cn>
2020-09-10 15:27:23 +0800

    ok

--- assets/js/login.js
+++ assets/js/login.js
@@ -29,12 +29,28 @@ $(function () {
     //监听注册表单提交
     $('#form-reg').on('submit', function (e) {
         e.preventDefault()
-        $.post('http://ajax.frontend.itheima.net/api/reguser', { username: $('#form-reg [name=username]').val(), password: $('#form-reg [name=password]').val() }, function (res) {
+        $.post('/api/reguser', { username: $('#form-reg [name=username]').val(), password: $('#form-reg [name=password]').val() }, function (res) {
             if (res.status !== 0) {
                 return layer.msg(res.message)
             }
-            layer.msg('注册成功')
+            layer.msg('注册成功,请登录')
         })
         $('#link-login').click()
+    })
+    $('#form-login').on('submit', function (e) {
+        e.preventDefault()
+        $.ajax({
+            url: '/api/login',
+            method: 'POST',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('res.message')
+                }
+                layer.msg('登录成功')
+                localStorage.setItem('token', res.token)
+                location.href = '/index.html'
+            }
+        })
     })
 })
